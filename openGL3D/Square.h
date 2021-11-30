06c460a30667543cab754ceb54539124f92bb12d
joseph <joedt100@gmail.com>
2021-11-30 15:08:20 +0000

    organised project nicely

--- openGL3D/Square.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<vector>
-#include "GameObject.h"
-class Square : public GameObject{
-	public:
-		Square(float width, float height, float x, float y, float z, float r, float g, float b, std::vector<GLfloat>* VBO, std::vector<GLuint>* EBO, const char* texturePath);
-};
