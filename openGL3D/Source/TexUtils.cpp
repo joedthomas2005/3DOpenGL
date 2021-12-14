8b6ad05b3d7c00ee0f1582ff7c3032e27cf78023
joe <joedt100@gmail.com>
2021-12-14 13:00:44 +0000

    conditional shading. not working rn

---
+++ openGL3D/Source/TexUtils.cpp
@@ -0,0 +1,38 @@
+#include "TexUtils.h"
+
+
+unsigned char* TexUtils::loadImage(std::string texPath, int* texWidth, int* texHeight, int* numChannels){
+	stbi_set_flip_vertically_on_load(true);
+	return stbi_load(texPath.c_str(), texWidth, texHeight, numChannels, 0);
+};
+
+
+GLenum TexUtils::colourSpace(std::string texPath){
+	std::ifstream imageSignature(texPath.c_str(), std::ios::in | std::ios::binary);
+	std::cout<<"Loading image "<<texPath<<std::endl;
+	char signature[128];
+	imageSignature.read(signature, 128);
+		
+	unsigned int hexsignature[128];
+		
+	for(int i = 0; i < 128; i++){
+		hexsignature[i] = (unsigned int)(unsigned char)(signature[i]);
+		std::cout<<std::hex<<hexsignature[i]<<",";	
+	}
+	std::cout<<std::endl;
+	if(hexsignature[0] == 0x89 && hexsignature[1] == 0x50 && hexsignature[2] == 0x4e && hexsignature[3] == 0x47){
+		return GL_RGBA;
+	}
+	else if(hexsignature[0] == 0xff && hexsignature[1] == 0xd8){
+		return GL_RGB;
+	}
+	else{
+		std::cout<<"ERROR UNRECOGNISED IMAGE FORMAT, MUST BE JPG OR PNG" << std::endl;
+		return 0;
+	}
+
+};
+
+void TexUtils::freeTexData(unsigned char* texData){
+	stbi_image_free(texData);
+}
