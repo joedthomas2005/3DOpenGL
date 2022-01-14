469a9d06a1eabb14cd123a7722a643368832f113
joseph <joedt100@gmail.com>
2022-01-14 10:15:55 +0000

    load cubes without vector

--- openGL3D/Headers/Cube.h
+++ openGL3D/Headers/Cube.h
@@ -12,6 +12,13 @@ public:
 		float r, float g, float b, 
 		std::vector<GLfloat> *VBO, std::vector<GLuint> *EBO, 
 		std::vector<const char*> texturePath);
+	
+	Cube(float x, float y, float z,
+		float width, float height, float depth,
+		float pitch, float yaw, float roll,
+		float r, float g, float b,
+		std::vector<GLfloat> *VBO, std::vector<GLuint> *EBO,
+		const char* right, const char* left, const char* top, const char* bottom, const char* front, const char* back);
 
 
 	void load(std::vector<GLfloat> *objVertices, std::vector<GLuint> *objIndices, std::vector<GLfloat> *VBOvector, std::vector<GLuint> *EBOvector, std::vector<const char*> texPaths);
--- openGL3D/Source/Cube.cpp
+++ openGL3D/Source/Cube.cpp
@@ -53,6 +53,59 @@ Cube::Cube(float x, float y, float z,
 
 }
 
+Cube::Cube(float x, float y, float z,
+	float width, float height, float depth,
+	float pitch, float yaw, float roll,
+	float r, float g, float b,
+	std::vector<GLfloat>* VBO, std::vector<GLuint>* EBO,
+	const char* right, const char* left, const char* top, const char* bottom, const char* front, const char* back)
+	:GameObject(x, y, z, pitch, yaw, roll, width, height, depth, false)
+{
+	std::vector<GLuint> indices = {
+		6, 4, 0, 0, 2, 6,		//FRONT
+		0, 1, 2, 2, 3, 1,		//RIGHT
+		7, 5, 1, 1, 3, 7,		//BACK
+		4, 5, 6, 6, 7, 5,		//LEFT
+		4, 0, 5, 5, 1, 0,		//TOP
+		6, 7, 2, 2, 3, 7		//BOTTOM
+	};
+	std::vector<GLfloat> verts;
+
+	/**
+	TOP RIGHT FRONT 0
+	TOP RIGHT BACK 1
+	BOTTOM RIGHT FRONT 2
+	BOTTOM RIGHT BACK 3
+	TOP LEFT FRONT 4
+	TOP LEFT BACK 5
+	BOTTOM LEFT FRONT 6
+	BOTTOM LEFT BACK 7
+	**/
+	for (int w = 1; w >= -1; w -= 2) {
+		for (int h = 1; h >= -1; h -= 2) {
+			for (int d = 1; d >= -1; d -= 2) {
+				verts.push_back(w * 0.25f);
+				verts.push_back(h * 0.25f);
+				verts.push_back(d * 0.25f);
+				verts.push_back(r);
+				verts.push_back(g);
+				verts.push_back(b);
+				verts.push_back(0);
+				verts.push_back(0);
+			}
+		}
+	}
+	std::cout << "CUBE VERTS" << std::endl;
+	for (GLfloat v : verts) {
+		std::cout << v << ",";
+	}
+	std::cout << std::endl;
+
+	this->load(&verts, &indices, VBO, EBO, std::vector<const char*>{right, left, top, bottom, front, back});
+
+
+}
+
 void Cube::load(std::vector<GLfloat> *objVertices, std::vector<GLuint> *objIndices, std::vector<GLfloat> *VBOvector, std::vector<GLuint>* EBOvector, std::vector<const char*> texturePaths){
 	
 	std::string textureName;
